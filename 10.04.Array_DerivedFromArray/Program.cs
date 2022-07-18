8fe93e8ce90c27be2cdbf0331a9ad5e2b02b1b32
com <com@DESKTOP-NVBQAUI>
2022-07-18 17:09:14 +0900

    220718. 10장 예제 1

---
+++ 10.04.Array_DerivedFromArray/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _10._04.Array_DerivedFromArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 10, 30, 20, 7, 1 };
+            Console.WriteLine($"Type Of array : {array.GetType()}");
+            Console.WriteLine($"Base type Of array : {array.GetType().BaseType}");
+        }
+    }
+}
---
+++ 10.05.MoreOnArray/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _10._05.MoreOnArray
+{
+    internal class Program
+    {
+        private static bool CheckPassed(int score)
+        {
+            return score >= 60;
+        }
+        private static void Print(int value)
+        {
+            Console.WriteLine($"{value} ");
+        }
+        static void Main(string[] args)
+        {
+            int[] scores = new int[] { 80, 74, 81, 90, 34 };
+            foreach (int score in scores)
+                Console.WriteLine($"{score} ");
+            Console.WriteLine();
+
+            Array.Sort(scores); //scores 내 값 오름차순 정렬
+            Array.ForEach<int>(scores, new Action<int>(Print));
+            Console.WriteLine();
+
+            Console.WriteLine($"Number of dimensions : {scores.Rank}");
+            Console.WriteLine($"Binary Search : 81 is at" + $"{Array.BinarySearch<int>(scores, 81)}"); //배열index
+
+            Console.WriteLine($"Linear Search : 90 is at" + $"{Array.IndexOf(scores, 90)}");
+            Console.WriteLine($"Everyone passed ?: " + $"{Array.TrueForAll<int>(scores, CheckPassed)}");
+
+            int index = Array.FindIndex<int>(scores, (score) => score < 60);
+            scores[index] = 61;
+            Console.WriteLine($"Everyone passed ?: " + $"{Array.TrueForAll<int>(scores, CheckPassed)}");
+            Console.WriteLine($"Old length of scores: " + $"{scores.GetLength(0)}");
+
+            Array.Resize<int>(ref scores, 10);
+            Console.WriteLine($"New length of scores: " + $"{scores.Length}");
+
+            Array.ForEach<int>(scores, new Action<int>(Print));
+            Console.WriteLine();
+
+            Array.Clear(scores, 3, 7);              //3번부터 7개 요소를 지우기?
+            Array.ForEach<int>(scores, new Action<int>(Print));
+
+            int[] sliced = new int[3];
+            Array.Copy(scores, 0, sliced, 0, 3);    //0번부터 3개 요소를 차례대로 복사
+            Array.ForEach<int>(sliced, new Action<int>(Print));
+            Console.WriteLine();
+        }
+    }
+}
