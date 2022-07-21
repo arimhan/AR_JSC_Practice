280e0111557246e5d70f4984dbe5d5d38d96baf3
com <com@DESKTOP-NVBQAUI>
2022-07-21 13:15:06 +0900

    220721 예제 18번까지 완료. 19번 일부내용만 예제 작성. 나머지는 우선순위 XX

--- 19.02.Synchronize/Program.cs
+++ 19.02.Synchronize/Program.cs
@@ -5,11 +5,9 @@ namespace _19._02.Synchronize
 {
     class Counter
     {
-        const int LOOP_COUNT = 10; // 1000
+        const int LOOP_COUNT = 1000;
         readonly object thisLock;
-        //readonly object thisLock2;
         private int count;
-        //private int testcount;
         public int Count
         {
             get { return count; }
@@ -19,24 +17,14 @@ namespace _19._02.Synchronize
             thisLock = new object();
             count = 0;
         }
-        //public int Test
-        //{
-        //    get { return testcount; }
-        //}
-        //public Tester()
-        //{
-        //    thisLock2 = new object();
-        //    testcount = 0;
-        //}
         public void Increase()
         {
             int loopCount = LOOP_COUNT;
-            while(loopCount-->0)
+            while(loopCount-- > 0)
             {
-                lock(thisLock)
+                lock (thisLock)
                 {
                     count++;
-                    //testcount++;
                 }
                 Thread.Sleep(1);
             }
@@ -49,7 +37,6 @@ namespace _19._02.Synchronize
                 lock (thisLock)
                 {
                     count--;
-                    //testcount--;
                 }
                 Thread.Sleep(1);
             }
@@ -60,16 +47,13 @@ namespace _19._02.Synchronize
         static void Main(string[] args)
         {
             Counter counter = new Counter();
-            //Tester tester = new Tester();
             Thread incThread = new Thread(new ThreadStart(counter.Increase));
             Thread decThread = new Thread(new ThreadStart(counter.Decrease));
             incThread.Start();
             decThread.Start();
             incThread.Join();
             decThread.Join();
-
             WriteLine(counter.Count);
-            //WriteLine(counter.Test);
         }
     }
 }
