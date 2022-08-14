224f552d6d0675649eb1e2adbee44e9316f670c4
arimhan <89959505+arimhan@users.noreply.github.com>
2022-08-14 23:21:33 +0900

    220814

---
+++ DelegateDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+//using static System.Console;
+
+namespace DelegateDemo
+{
+    class Program
+    {
+        delegate float SumHandler(float a, float b);
+        SumHandler sumHandler;
+
+        private float Sum(float a, float b)
+        {
+            return a + b;
+        }
+        private void Start()
+        {
+            sumHandler = Sum;
+            float sum = sumHandler(10.0f, 5.0f);
+            Console.WriteLine($"Sum = {sum}");
+        }
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Start();
+        }
+    }
+}
